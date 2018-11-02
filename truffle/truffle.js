ababc92a0577086da7fffacabce0778cb6b4b543
Sachit <sachit.shetty@gmail.com>
2018-11-01 23:02:48 -0400

    Blockchain on Azure Demo

---
+++ truffle/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "labfbd-dns-reg1.eastus.cloudapp.azure.com:8545",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 4712388
+    }
+  }
+};
